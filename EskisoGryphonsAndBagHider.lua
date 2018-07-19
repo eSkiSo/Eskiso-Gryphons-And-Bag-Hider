569476f94db837c4595e1e4d528d9f45e95177ee
Eskiso <eSkiSo@users.noreply.github.com>
2018-07-19 14:09:18 +0100

    First commit

---
+++ EskisoGryphonsAndBagHider.lua
@@ -0,0 +1,8 @@
+-- This file is loaded from "EskisoGryphonsAndBagHider.toc"
+
+-- Hide Gryphons
+MainMenuBarArtFrame.LeftEndCap:Hide()
+MainMenuBarArtFrame.RightEndCap:Hide()
+
+-- Hide Bags
+MicroButtonAndBagsBar:Hide()
